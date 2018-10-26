31bc50c73ad046974c94ed5804e41af69534703a
younganakin <joenjuguna53@gmail.com>
2018-10-26 16:55:53 +0300

    Add django cors.

--- mysite/settings.py
+++ mysite/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = ['localhost',
 
 INSTALLED_APPS = [
     'ads.apps.AdsConfig',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -168,3 +170,6 @@ ADS_ZONES = {
         'google_adsense_format': 'auto',  # OPTIONAL - DEFAULT TO None
     },
 }
+
+
+CORS_ORIGIN_ALLOW_ALL = True
