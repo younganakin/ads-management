6d8ff93d2221473225a9b9c3847dead2c56e2516
younganakin <joenjuguna53@gmail.com>
2018-11-05 08:16:09 +0300

    Fix Click and Impersions.

--- ads/views.py
+++ ads/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponseRedirect
+from django.conf import settings
 from django.shortcuts import render
 from django.views import View
+from django.utils import timezone
 from django.views.generic.detail import SingleObjectMixin
 
-from ads.models import Ad, Click
+from ads.models import Ad, Click, Impression
 from ads.utils import get_client_ip
 
 
@@ -27,8 +29,20 @@ class AdClickView(SingleObjectMixin, View):
 def random_ad(request, zone, group):
     # Retrieve random ad for the zone based on weight
     ad = Ad.objects.random_ad(zone, group)
+
+    if ad is not None:
+        if request.session.session_key:
+            impression, created = Impression.objects.get_or_create(
+                ad=ad,
+                session_id=request.session.session_key,
+                defaults={
+                    'impression_date': timezone.now(),
+                    'source_ip': get_client_ip(request),
+                })
+
     context = {
         'ad': ad,
+        'zone': settings.ADS_ZONES.get(zone, None)
     }
     return render(request, 'ads/random-ad.html', context)
 
@@ -36,7 +50,19 @@ def random_ad(request, zone, group):
 def static_ad(request, title, zone):
     # Retrieve random ad for the zone based on weight
     ad = Ad.objects.static_ad(title, zone)
+
+    if ad is not None:
+        if request.session.session_key:
+            impression, created = Impression.objects.get_or_create(
+                ad=ad,
+                session_id=request.session.session_key,
+                defaults={
+                    'impression_date': timezone.now(),
+                    'source_ip': get_client_ip(request),
+                })
+
     context = {
         'ad': ad,
+        'zone': settings.ADS_ZONES.get(zone, None)
     }
     return render(request, 'ads/static-ad.html', context)
